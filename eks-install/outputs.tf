36972dd38ac2f30af0a98063e61de91ba57b73d1
RohithKumarKN <knrohithkumar@gmail.com>
2025-11-16 17:20:23 +0530

    Fixed output.

--- eks-install/outputs.tf
+++ eks-install/outputs.tf
@@ -2,28 +2,11 @@ output "cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
-
 output "cluster_name" {
   description = "EKS cluster name"
   value       = module.eks.cluster_name
 }
-
 output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
-}
-
-output "private_subnet_ids" {
-  description = "Private subnet IDs"
-  value       = module.vpc.private_subnet_ids
-}
-output "public_subnet_ids" {
-  description = "Public subnet IDs"
-  value       = module.vpc.public_subnet_ids
-}
-
-output "node_group_names" {
-  description = "EKS Node Group Names"
-  value       = var.node_groups[*].name
-
 }
